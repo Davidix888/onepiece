c75552ef7be405ff3d32d9ab09219f3052600d5d
David Ixquiac <davidixquiac212@gmail.com>
2023-02-14 09:47:57 -0600

    Se agrego modulos al .gitignore

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+print('Luis David Ixquiac Sac')
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
+
+
